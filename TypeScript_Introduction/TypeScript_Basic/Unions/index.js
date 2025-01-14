656c45cbfe54f23eea6de8096185850e5b84eb22
mangtrungzos <sangv906@gmail.com>
2025-01-14 18:52:19 +0700

    fix

--- TypeScript_Introduction/TypeScript_Basic/Unions/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// `unions` là một kiểu dữ liệu cho phép một biến hoặc một đối tượng có thể nhận nhiều kiểu khác nhau.
-// Điều này rất hữu ích khi bạn cần linh hoạt về kiểu dữ liệu mà biến hoặc tham số có thể nhận.
-// syntax: |
-let value;
-value = "Ngan";
-value = 2002;
-let userRole = "member";
